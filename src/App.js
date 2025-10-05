433def4a9e7cc16619046d8dcc669ba3e3519380
Deinmotei Inifiye-Israel <deinmoteiinifiyeisrael1@gmail.com>
2025-10-05 21:41:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Banner, About, Products, Contact}
+from "./index.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+    <>
+    {/* Simple Navigation */}
+  <nav> 
+    <a href="#banner">Home </a> |{""}
+    <a href="#about">About </a> |{""}
+    <a href="#products">Products</a> |{""}
+    <a href="#contact">Contact</a> 
+    </nav>
 
+    {/* Page Sections */}
+    <Banner />
+    <About />
+    <Products />
+    <Contact />
+    </>
+  );
+} 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+export const Banner= () => (
+  <section id= "banner">
+    <h1>Welcome to my SPA</h1>
+    <p> It's a simple site still a work in progress but we getting there.</p>
+  </section>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const About = () => (
+  <section id="about">
+    <h1>What's this site about?</h1>
+    <p>A sample site for the assignment given by Mr Promise champion</p>
+  </section>
+);
+export const Products = () => (
+  <section id ="products">
+  <h1>My services</h1>
+  <p> front-send dev </p>
+  <p> react developer</p>
+  </section>
+)
+export const Contact = () => (
+  <section id = "contact">
+    <h1>Contact us</h1>
+    <p>Email : deinmoteiinifiyeisrael1@gmail.com</p>
+    <p>Phone number : 07025198739</p>
+  </section>
+); 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
